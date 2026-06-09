cac2ba46e57e8e33e58337c165ba2283f5ddc4a5
chakriturium <chakradhar.cherukuri@turium.ai>
2026-06-09 17:15:44 +0000

    Revert "Update:Frappe to Orbit7"
    This reverts commit 218f787896e2d9bf0d50ceb77f2c241b7e12079a.

--- insights_extension/doctype/test_insights_sidebar.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, abc and Contributors
-# See license.txt
-
-# import orbit7
-from orbit7.tests.utils import FrappeTestCase
-
-
-class TestInsightsSidebar(orbit7TestCase):
-	pass
